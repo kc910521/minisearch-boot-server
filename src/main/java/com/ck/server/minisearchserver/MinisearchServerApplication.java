79b46743912b07d0c377a4173b9c66fd7f5cb646
caikun1 <caikun@baidu-mgame.com>
2021-01-27 19:48:04 +0800

    boot server

---
+++ src/main/java/com/ck/server/minisearchserver/MinisearchServerApplication.java
@@ -0,0 +1,29 @@
+package com.ck.server.minisearchserver;
+
+import com.ck.common.mini.config.MiniSearchServer;
+import com.ck.common.mini.spring.DefaultMiniSearchSpringConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.Assert;
+
+@SpringBootApplication
+@MiniSearchServer
+@Configuration
+@Import(DefaultMiniSearchSpringConfig.class)
+public class MinisearchServerApplication {
+
+
+    public MinisearchServerApplication(RedisTemplate<String, String> redisTemplate, DefaultMiniSearchSpringConfig defaultMiniSearchSpringConfig
+                                       ) {
+        assert redisTemplate != null;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinisearchServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ck/server/minisearchserver/MinisearchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ck.server.minisearchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinisearchServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
